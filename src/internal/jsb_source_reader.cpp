d026234b6e241eebdcb104839ce543c1e0ee7a96
忘忧の-Daylily_Zeleen <daylily-zeleen@foxmail.com>
2026-08-17 06:09:41 +0800

    正确处理 FileAccessSourceReader 打开文件失败时的情况，防止崩溃

--- src/internal/jsb_source_reader.cpp
+++ src/internal/jsb_source_reader.cpp
@@ -5,10 +5,15 @@
 namespace jsb::internal {
 FileAccessSourceReader::FileAccessSourceReader(const String &p_file_name) {
 	file_ = FileAccess::open(p_file_name, FileAccess::READ);
-	cached_length_ = file_.is_null() ? 0 : file_->get_length();
-
-	String path_absolute = file_->get_path_absolute();
-	source_url = path_absolute.is_empty() ? p_file_name : path_absolute;
+	if (file_.is_valid()) {
+		cached_length_ = file_->get_length();
+		source_url = file_->get_path_absolute();
+	} else {
+		// 偶发性的 ERR_CAN_NOT_OPEN.
+		JSB_LOG(Error, "Failed to open file: %s - %s", p_file_name, UtilityFunctions::error_string(FileAccess::get_open_error()));
+		cached_length_ = 0;
+		source_url = p_file_name;
+	}
 }
 
 StringSourceReader::StringSourceReader(const String &p_path, const String &p_absolute_path, const String &p_source)
