95eab2f53791d7dc1ab47b65b5157b0d47cc5d93
忘忧の-Daylily_Zeleen <daylily-zeleen@foxmail.com>
2026-08-19 23:36:51 +0800

    部分文件添加版权声明

--- scripts/typings/godot.shadowRealm.d.ts
+++ scripts/typings/godot.shadowRealm.d.ts
@@ -1,7 +1,30 @@
+/*
+ *  godot.shadowRealm.d.ts
+ *
+ *  This file is part of:
+ *                                GodotJS-Ext
+ *              https://github.com/Daylily-Zeleen/GodotJS-Ext
+ *
+ *  Copyright (c) 2026-present 忘忧の (Daylily-Zeleen)
+ *                 - Contact: daylily-zeleen@foxmail.com
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * 跨 Realm 访问对象的 symbol 键时，如果不是全局或内置的 symbol 将无法正常访问
- * 
- * @NOTE: 当前不支持在 ShadowRealm 中创建 Godot 对象
  */
 declare module "godot.shadowRealm" {
     import { GAny, GArray } from "godot";
--- src/api_tool/editor/api_tool_generator.h
+++ src/api_tool/editor/api_tool_generator.h
@@ -1,3 +1,28 @@
+/************************************************************************/
+/*  api_tool_generator.h                                                */
+/************************************************************************/
+/*  This file is part of:                                               */
+/*                                GodotJS-Ext                           */
+/*              https://github.com/Daylily-Zeleen/GodotJS-Ext           */
+/*                                                                      */
+/*  Copyright (c) 2026-present 忘忧の (Daylily-Zeleen)                  */
+/*                 - Contact: daylily-zeleen@foxmail.com                */
+/*                                                                      */
+/*  This library is free software; you can redistribute it and/or       */
+/*  modify it under the terms of the GNU Lesser General Public          */
+/*  License as published by the Free Software Foundation; either        */
+/*  version 2.1 of the License, or (at your option) any later version.  */
+/*                                                                      */
+/*  This library is distributed in the hope that it will be useful,     */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of      */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   */
+/*  Lesser General Public License for more details.                     */
+/*                                                                      */
+/*  You should have received a copy of the GNU Lesser General Public    */
+/*  License along with this library; if not,                            */
+/*  see <https://www.gnu.org/licenses/>.                                */
+/************************************************************************/
+
 #pragma once
 
 // editor/api_tool_generator.h
