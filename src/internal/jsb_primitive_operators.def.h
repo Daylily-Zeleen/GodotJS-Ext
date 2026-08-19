e1ef9b6203bc1101ebeb78ba65b115cdc794cac5
忘忧の-Daylily_Zeleen <daylily-zeleen@foxmail.com>
2026-08-20 00:07:09 +0800

    剩余文件添加版权声明

--- src/internal/jsb_primitive_operators.def.h
+++ src/internal/jsb_primitive_operators.def.h
@@ -1,4 +1,31 @@
-﻿// Godot do not make operator list public, so we can only do it manually
+﻿/************************************************************************/
+/*  jsb_primitive_operators.def.h                                       */
+/************************************************************************/
+/*  This file is part of:                                               */
+/*                                GodotJS-Ext                           */
+/*              https://github.com/Daylily-Zeleen/GodotJS-Ext           */
+/*                                                                      */
+/*  Copyright (c) 2026-present 忘忧の (Daylily-Zeleen)                  */
+/*                 - Contact: daylily-zeleen@foxmail.com                */
+/*  Copyright (c) Contributors of GodotJS                               */
+/*                 - <https://github.com/godotjs/GodotJS>               */
+/*                                                                      */
+/*  This library is free software; you can redistribute it and/or       */
+/*  modify it under the terms of the GNU Lesser General Public          */
+/*  License as published by the Free Software Foundation; either        */
+/*  version 2.1 of the License, or (at your option) any later version.  */
+/*                                                                      */
+/*  This library is distributed in the hope that it will be useful,     */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of      */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   */
+/*  Lesser General Public License for more details.                     */
+/*                                                                      */
+/*  You should have received a copy of the GNU Lesser General Public    */
+/*  License along with this library; if not,                            */
+/*  see <https://www.gnu.org/licenses/>.                                */
+/************************************************************************/
+
+// Godot do not make operator list public, so we can only do it manually
 // TODO: 通过 extension_api.json 进行代码生成，怎么会有傻逼进行硬编码
 
 JSB_TYPE_BEGIN(Vector2)
